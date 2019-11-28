a072addecb9ee1a4289e71f3302dd27973a3ecf3
Nabin Karki <naabin@outlook.com>
2019-11-29 01:00:56 +1100

    Added Spring Data JPA. Created Reservation repository, service and tests.

---
+++ reservation/src/main/java/com/reservation/repositories/ReservationRepository.java
@@ -0,0 +1,12 @@
+package com.reservation.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.reservation.models.Reservation;
+
+
+@Repository
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+}
---
+++ reservation/src/main/java/com/reservation/services/impl/ReservationServiceImpl.java
@@ -0,0 +1,50 @@
+package com.reservation.services.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.reservation.models.Reservation;
+import com.reservation.services.ReservationService;
+
+
+@Service
+public class ReservationServiceImpl implements ReservationService {
+
+	@Override
+	public Reservation createReservation(Reservation reservation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Reservation updateReservation(Reservation reservation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Reservation> getAllReservations() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Reservation findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteReservationById(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteReservation(Reservation reservation) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
